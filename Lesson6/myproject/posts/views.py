4ffde574bf865c13bcd13353ed8ccf8a10ca1a11
Om Patel <ompatel2019@gmail.com>
2024-10-31 19:30:38 +1100

    completed lesson6

--- Lesson6/myproject/posts/views.py
+++ Lesson6/myproject/posts/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from . models import Post
 
-# Create your views here.
+# View for listing all posts
 def posts_list(request): 
     posts = Post.objects.all().order_by('-date')    
     return render(request, 'posts/posts_list.html', {'posts': posts})
 
+# View for displaying a single post based on slug
+def post_page(request, slug):
+    post = get_object_or_404(Post, slug=slug) 
+    return render(request, 'posts/post_page.html', {'post': post})
