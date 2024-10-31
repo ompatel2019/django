91e5e9f1d2208ed47161710c1ac04827b3853f98
Om Patel <ompatel2019@gmail.com>
2024-10-31 15:35:15 +1100

    lesson5 boiler plate

--- Lesson3/myproject/posts/models.py
+++ Lesson3/myproject/posts/models.py
@@ -2,8 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length = 75) 
+    title = models.CharField(max_length=75) 
     body = models.TextField()
     slug = models.SlugField()
-    date = models.DateTimeField(auto_now_add = True)
-    name = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
