9d47fcb8ad2393d90f0f02bdc2d7b69e278919f6
Om Patel <ompatel2019@gmail.com>
2024-11-01 22:08:46 +1100

    login and auth

--- Lesson7/myproject/users/views.py
+++ Lesson7/myproject/users/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 
 # Create your views here.
 def register(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            login(request, form.save())
             return redirect("posts:list")
     else:
         form = UserCreationForm()
     return render(request, 'users/register.html', {"form": form})
+
+def user_login(request): 
+    if request.method == "POST": 
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect("posts:list")
+    else: 
+        form = AuthenticationForm()
+    return render(request, 'users/login.html', {"form": form})
